e5cddd3ea5201ef88f04ad6790286e27a0c98886
msxiao <msxiao@Ctrip.com>
2015-09-18 10:09:23 +0800

    test

--- mod/notifier.js
+++ mod/notifier.js
@@ -1,21 +1,35 @@
 cFly.define("cFly.notifier", function () {
-    var subscribeCache = {},
-        tokenPrefix = "__subscribe__";
+    var _cache = {},
+        tokenPrefix = "";
 
     /**
      * 发布通知
-     * @param {String} what 通知类型
+     * @param {String} type 通知类型
      * @param {Object} args 通知数据
      */
-    var publish = function (what, args) {
-        var callbacks = subscribeCache[what] || [];
-        if (callbacks.length > 0) {
-            var i, info = callbacks[0], len = callbacks.length;
-            for (i = 0; i < len; callback = info[++i]) {
-                if (info && info.callback) {
-                    setTimeout(function () {
-                        info.callback.call(info.context, args);
-                    }, 0);
+    var notify = function (type, args) {
+        var item = _cache[type],
+            handlers;
+
+        if (!item) {
+            item = _cache[type] = {
+                lastData: null,
+                handlers: []
+            };
+        }
+        handlers = item.handlers;
+        //记录最后一次发送的数据
+        item.lastData = args;
+
+        if (handlers.length > 0) {
+            var i = 0, info;
+            while (info = handlers[i++]) {
+                if (info.handler) {
+                    try {
+                        info.handler.call(info.context, args);
+                    } catch (ex) {
+
+                    }
                 }
             }
         }
@@ -23,67 +37,93 @@ cFly.define("cFly.notifier", function () {
 
     /**
      * 订阅通知
-     * @param {String} what 通知类型
+     * @param {String|Array} types 通知类型
      * @param {Function} callback 通知回调函数
      * @param {Object} context callback执行环境
+     * @param {Boolean} exeLastData 是否执行最后一次通知事件
      * @returns {String} token:订阅号
      */
-    var subscribe = function (what, callback, context) {
-        if (!callback) {
-            return;
+    var register = function (types, handler, context, execLastData) {
+        if (!types || !handler) {
+            return false;
         }
-        var token = cFly.guid(tokenPrefix),
-            callbacks = subscribeCache[what];
-        var info = {
-            token: token,
-            callback: callback,
-            context: context
-        };
 
-        if (!callbacks) {
-            callbacks = [];
+        //fix argument
+        types = cFly.makeArray(types);
+        if (typeof context === "boolean") {
+            execLastData = context;
+            context = null;
         }
-        callbacks.push(info);
+
+        for (var i = 0, l = types.length; i < l; i++) {
+            var type = types[i],
+                handlers,
+                initialized = true,
+                token = cFly.guid(tokenPrefix),
+                item = _cache[type];
+            if (!item) {
+                initialized = false;
+                item = _cache[type] = {
+                    lastData: null,
+                    handlers: []
+                };
+            }
+            handlers = item.handlers;
+            var info = {
+                token: token,
+                handler: handler,
+                context: context
+            };
+            handlers.push(info);
+
+            if (execLastData && initialized) {
+                try {
+                    info.handler.call(info.context, item.lastData);
+                } catch (ex) {
+
+                }
+            }
+        }
         return token;
     };
 
     /**
      * 退订通知
-     * notifier.unsubscribe("CHANGE_EVENT",onChange)  根据what,callback退订
-     * notifier.unsubscribe("__subscribe__12343")  根据订阅号退订
+     * notifier.remove("CHANGE_EVENT",onChange)  根据what,callback退订
+     * notifier.remove(10343)  根据订阅号退订
      */
-    var unsubscribe = function () {
+    var remove = function () {
         var args = cFly.makeArray(arguments);
         if (args.length === 0) {
             return;
         }
-        var what = args[0],
+        var type = args[0],
             token = args.length === 1 && args[0],
             callback = args.length > 1 && args[1];
 
-        var internalRemove = function (what, type, value) {
-            var callbacks = subscribeCache[what],
-                len = callbacks ? callbacks.length : 0;
+        var internalRemove = function (type, removeType, value) {
+            var item = _cache[type],
+                handlers = item.handlers,
+                i = handlers ? handlers.length - 1 : 0,
+                info;
 
-            if (len > 0) {
-                for (var i = len - 1, info = callbacks[i]; i > 0; info = callbacks[--i]) {
-                    if (info && info[type] === value) {
-                        callbacks.splice(i, 1);
-                    }
+            while (info = handlers[i--]) {
+                if (info && info[removeType] === value) {
+                    handlers.splice(i, 1);
                 }
             }
         };
         if (token) {
-            for (what in subscribeCache) {
-                internalRemove(what, "token", token);
+            for (type in _cache) {
+                internalRemove(type, "token", token);
             }
         } else {
-            internalRemove(what, "callback", callback);
+            internalRemove(type, "callback", callback);
         }
     };
     return {
-        publish: publish,
-        subscribe: subscribe,
-        unsubscribe: unsubscribe
+        notify: notify,
+        register: register,
+        remove: remove
     };
 });
